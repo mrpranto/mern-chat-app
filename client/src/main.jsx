7b86ba1a252ebf2ed80a68167d5a5338589019e9
mrpranto <prantoabir1@gmail.com>
2024-12-21 21:52:42 +0600

    socket setup frontend and backend both done

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { Toaster } from './components/ui/sonner'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Toaster } from "./components/ui/sonner";
+import { SocketProvider } from "./context/SocketContex";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   // <StrictMode>
-  <>
-   <App />
-   <Toaster />
-  </>
+  <SocketProvider>
+    <App />
+    <Toaster />
+  </SocketProvider>
   // </StrictMode>,
-)
+);
--- server/socket.js
+++ server/socket.js
@@ -30,7 +30,7 @@ const setupSocket = (server) => {
             console.log("User ID not provided during connection.");
         }
 
-        socket.on("disconnected", () => disconnect(socket));
+        socket.on("disconnect", () => disconnect(socket));
     })
 
 }
